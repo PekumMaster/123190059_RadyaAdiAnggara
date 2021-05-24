34986e21ce158982d96891b6d5ed26ddbbc0d793
PekumMaster <81898945+PekumMaster@users.noreply.github.com>
2021-05-24 21:21:28 +0700

    Add files via upload

---
+++ Rental/lengkap/src/Rental/Kendaraan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Rental;
+
+/**
+ *
+ * @author GF63-9SC-621ID
+ */
+public abstract class Kendaraan {
+    String merk;
+    int day;
+
+    public Kendaraan(String merk, int day) {
+        this.merk = merk;
+        this.day = day;
+    }
+    
+    abstract int cost();
+}
