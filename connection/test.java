a22ad65d7bb94329938e9e131e8266dbdc0da3aa
PekumMaster <81898945+PekumMaster@users.noreply.github.com>
2021-06-21 23:17:07 +0700

    Add files via upload

---
+++ connection/test.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connection;
+
+/**
+ *
+ * @author GF63-9SC-621ID
+ */
+public class test {
+    
+}
